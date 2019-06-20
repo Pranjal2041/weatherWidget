079c9ba9d7eee004bf8e5ef26ed244644fa41306
pranjal2041 <pranjal2041@gmail.com>
2019-06-20 18:11:48 +0530

    Switch between themes and renames

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
+import static com.example.myapplication.HomeActivity.MyPREFERENCES;
+
+public class Constants extends AppCompatActivity {
+
+
+
+
+
+}

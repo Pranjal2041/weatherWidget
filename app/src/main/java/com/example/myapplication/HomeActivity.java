38a28a128489b5d22786578a712a3087b578238f
Salazar-prime <varun.aggarwal@iitgn.ac.in>
2019-06-24 22:13:05 +0530

    begin app redesign
    first design

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,20 +1,24 @@
 package com.example.myapplication;
 
+import android.annotation.TargetApi;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -22,6 +26,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.github.florent37.awesomebar.ActionItem;
+import com.github.florent37.awesomebar.AwesomeBar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +43,7 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
     SharedPreferences sharedpreferences;
 
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,9 +51,9 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Transparent Status bar @Salazar
         Window window = getWindow();
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.setStatusBarColor(Color.TRANSPARENT);
+        window.setStatusBarColor(getColor(R.color.eo_dblue));
 
         // init SharedPreference
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
@@ -76,6 +84,24 @@ public class HomeActivity extends AppCompatActivity implements AdapterView.OnIte
         snapHelper.attachToRecyclerView(recyclerView);
         /* end recyclerView*/
 
+        // AwesomeBar
+        AwesomeBar bar = findViewById(R.id.bar);
+//        bar.addAction(R.drawable.awsb_ic_edit_animated, "");
+
+        bar.setActionItemClickListener(new AwesomeBar.ActionItemClickListener() {
+            @Override
+            public void onActionItemClicked(int position, ActionItem actionItem) {
+                Toast.makeText(getBaseContext(), actionItem.getText()+" clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+//        bar.setOnMenuClickedListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.openDrawer(Gravity.START);
+//            }
+//        });
+
+        bar.displayHomeAsUpEnabled(false);
 
         // Spinner - select widget for home screen
         Spinner spinner=(Spinner)findViewById(R.id.simpleSpinner);

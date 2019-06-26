7002743f4990cb6c872c0913f02c9d7b7b1b78e9
Salazar-prime <varun.aggarwal@iitgn.ac.in>
2019-06-26 20:51:54 +0530

    update nav_menu_icon animation

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -58,7 +58,7 @@ public class HomeActivity extends AppCompatActivity {
         // AppBar @Salazar
         drawerLayout = findViewById(R.id.drawer_layout);
         iconMenu = (ImageView) findViewById(R.id.ab_ic_menu);
-        iconMenu.setImageDrawable(AnimatedVectorDrawableCompat.create(this, R.drawable.avd_anim_menu_animated));
+        iconMenu.setImageDrawable(AnimatedVectorDrawableCompat.create(this, R.drawable.anic_menu));
         menuAnimationHandler.sendEmptyMessageDelayed(MESSAGE_ANIMATION_START, DELAYT_BEFORE_FIRST_MENU_ANIMATION);
         iconMenu.setOnClickListener(new View.OnClickListener() {
             @Override
